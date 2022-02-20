88a973f86212d67bee4c94ab6ad8688ae4844ac1
MUIAPP <muiapp@muilib.com>
2022-02-20 18:06:22 +0800

    增加了打开指定服务器控制面板的功能

--- firefox/data/action/index.js
+++ firefox/data/action/index.js
@@ -35,20 +35,61 @@ function detail() {
 			});
 		} 
 		else {
-			browser.storage.local.get(config.command.guess, function(item) {
-				var ariangUrl = "../../data/ariang/index.html"
-				if (item.autoSet) {
-					ariangUrl += "#!/settings/rpc/set/";
-					ariangUrl += (item.protocol + "/" + item.host + "/" + item.port + "/" + 
-					item.interf + "/" + btoa(item.token));
+			var rpc = document.querySelector(".s1").value;
+			var autoSetNG = false;
+
+			browser.storage.local.get(Object.assign(config.command.guess), item =>{
+				autoSetNG = item.autoSet;
+				//console.log("0:" + autoSetNG);
+				if(rpc == "1")
+				{
+					//console.log("1:" + autoSetNG);
+					var ariangUrl = "../../data/ariang/index.html";
+					if (autoSetNG) {
+						ariangUrl += "#!/settings/rpc/set/";
+						ariangUrl += (item.protocol + "/" + item.host + "/" + item.port + "/" + 
+						item.interf + "/" + btoa(item.token));
+					}
+					//console.log("1:" + ariangUrl);
+					browser.tabs.create({
+						url: ariangUrl
+					});
+					window.close();
+				} 
+				else if(rpc == "2") {
+					//console.log("2:" + autoSetNG);
+					browser.storage.local.get(config.command.s2, function(item) {
+						var ariangUrl = "../../data/ariang/index.html";
+						if (autoSetNG) {
+							ariangUrl += "#!/settings/rpc/set/";
+							ariangUrl += (item.protocol2 + "/" + item.host2 + "/" + item.port2 + "/" + 
+							item.interf2 + "/" + btoa(item.token2));
+						}
+						//console.log("2:" + ariangUrl);
+						browser.tabs.create({
+							url: ariangUrl
+						});
+						window.close();
+					});
 				}
-				browser.tabs.create({
-					url: ariangUrl
-				});
-				window.close();
+				else if(rpc == "3") {
+					browser.storage.local.get(config.command.s3, function(item) {
+						var ariangUrl = "../../data/ariang/index.html";
+						if (autoSetNG) {
+							ariangUrl += "#!/settings/rpc/set/";
+							ariangUrl += (item.protocol3 + "/" + item.host3 + "/" + item.port3 + "/" + 
+							item.interf3 + "/" + btoa(item.token3));
+						}
+						//console.log("3:" + ariangUrl);
+						browser.tabs.create({
+							url: ariangUrl
+						});
+						window.close();
+					});
+				}
 			});
 		}
-	});
+	});	
 }
 
 function launch() {
@@ -59,6 +100,26 @@ function launch() {
 	});
 	browser.storage.local.get("enabled", function(item) {
 		document.getElementById('switch').checked = item.enabled;
+	});
+
+	browser.storage.local.get(Object.assign(config.command.guess), prefs => {
+		var stext = document.querySelector(".s1").options[0].text;
+		if(prefs.sname == "")
+			document.querySelector(".s1").options[0].text = prefs.host  + ":" + prefs.port + " - " + stext;
+		else
+			document.querySelector(".s1").options[0].text = prefs.sname + " - " + stext;
+	});
+	browser.storage.local.get(Object.assign(config.command.s2), prefs => {
+		if(prefs.sname2 == "")
+			document.querySelector(".s1").options[1].text = prefs.host2 + ":" + prefs.port2;
+		else
+			document.querySelector(".s1").options[1].text = prefs.sname2;
+	});
+	browser.storage.local.get(Object.assign(config.command.s3), prefs => {
+		if(prefs.sname3 == "")
+			document.querySelector(".s1").options[2].text = prefs.host3 + ":" + prefs.port3;
+		else
+			document.querySelector(".s1").options[2].text = prefs.sname3;
 	});
 }
 //document.addEventListener('WebComponentsReady', launch, false);
